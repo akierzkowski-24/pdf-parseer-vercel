674271664775410d1796d791c7bc9ee99bdb05fb
akierzkowski-24 <akierzkowski24@gmail.com>
2025-09-10 16:16:35 +0200

    Update parse-transcript.py

--- api/parse-transcript.py
+++ api/parse-transcript.py
@@ -1,9 +1,5 @@
-# api/parse-transcript.py
-from http.server import BaseHTTPRequestHandler
-
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-Type', 'text/plain')
-        self.end_headers()
-        self.wfile.write(b"Hello from Vercel!")
+def handler(request):
+    return {
+        "statusCode": 200,
+        "body": "Hello from Vercel Python!"
+    }
